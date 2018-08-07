3ef7e116c0e5f5999112ff771788e1ea0ff5b8bc
VerVerean <42184876+VerVerean@users.noreply.github.com>
2018-08-07 13:46:40 -0400

    Updated

--- spigot/src/main/java/io/github/xxyy/cmdblocker/spigot/config/SpigotAliasResolver.java
+++ spigot/src/main/java/io/github/xxyy/cmdblocker/spigot/config/SpigotAliasResolver.java
@@ -31,6 +31,7 @@ import java.lang.reflect.Field;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
+import java.util.Collection;
 
 /**
  * A command alias resolver for Spigot server. Also supports Minecraft and Bukkit commands using reflection.
